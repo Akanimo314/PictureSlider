79890db22f1da75142b8f1d1f6869c9707e0c1b3
Akanimo <ekanemakanimo10@gmail.com>
2023-04-12 09:09:51 +0100

    PictureSlider

---
+++ java/slid.js
@@ -0,0 +1,18 @@
+
+const imageVar = document.getElementById("image");
+
+const imageArrays = ["./image/slid5.jpg", "./image/slid6.jpg", "./image/slid 1.webp", 
+"./image/slid 2.webp", "./image/slid 3.webp", "./image/slid 4.webp"]
+
+let imageIndex = 0
+
+function slideShow(){
+    imageVar.setAttribute("src", imageArrays[imageIndex])
+    imageIndex++
+
+    if(imageIndex >= imageArrays.length){
+        imageIndex = 0
+    }
+}
+
+setInterval(slideShow, 3000)
